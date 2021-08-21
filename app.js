23d8389690650cc78821af8fe6eb63413d39a392
dragonthakur2107 <hemanshsinghthakur@gmail.com>
2021-08-21 19:02:23 +0530

    completed

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
+const birthDate = document.querySelector("#birth-Date");
+const luckyNumber = document.querySelector("#lucky-Number");
+const resultArea = document.querySelector("#result-Area");
 
+
+const btnCheck = document.querySelector("#btn-Main")
+
+
+btnCheck.addEventListener("click",function dated(){
+    const dateOne = birthDate.value;
+    const luckNo = luckyNumber.value;
+    const sumOfDate=bdayCalci(dateOne);
+    if( sumOfDate % luckNo ===0 ){
+    resultArea.innerText= "Yup! Your Birthdate is lucky.";
+    }
+    else{
+        resultArea.innerText="Your Birthdate is not so lucky.";
+    }
+});
+function dateformatter(dob){
+    dob=dob.replaceAll("-","")
+    return dob;
+} 
+function bdayCalci(dob){
+    const dateTwo=dateformatter(dob);
+    let sum=0;
+    for(let i=0;i<dateTwo.length;i++)
+    {
+        sum=sum+ Number(dateTwo.charAt(i));
+    }
+    return sum;
+}
